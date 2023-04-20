dda27728b73382c70ada2e3bb30f32c2b1270fff
dswdcdm <dswdcdm@gmail.com>
2023-04-20 11:28:16 +0800

    update

--- codeigniter4/app/Views/components/Footer.php
+++ codeigniter4/app/Views/components/Footer.php
@@ -1,3 +1,4 @@
 <em>&copy;copyright 2023 - 2023 </em>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
