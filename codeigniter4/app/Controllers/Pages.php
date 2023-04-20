e57024508092798727faca83e2bc8cb051bcbd75
dswdcdm <dswdcdm@gmail.com>
2023-04-20 11:28:37 +0800

    update

--- codeigniter4/app/Controllers/Pages.php
+++ codeigniter4/app/Controllers/Pages.php
@@ -11,6 +11,10 @@ class Pages extends BaseController
 
     public function view($page = 'home')
     {
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+           /*  throw new PageNotFoundException($page); */
+        }
         
         $data['title'] = ucfirst($page); // Capitalize the first letter
 
